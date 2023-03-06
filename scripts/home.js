beb95451d284aa1a294d730422456bbfb8a6eba8
inebenitez <101297551+inebenitez@users.noreply.github.com>
2023-03-05 22:18:40 -0300

    search/category

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,15 @@
 let card = document.getElementById("card-template");
 
-const cards = data.events.map((x) => {
-  return `
+function mostrarCards(arr) {
+  const cards = arr.map((x) => {
+    return `
     <div class="col card2" id="${x._id}">
       <div class="card h-100">
         <img src="${x.image}" class="card-img-top" style="width:100% ; height:15vw ; object-fit:cover;" alt="...">
         <div class="card-body  d-flex flex-column">
           <h5 class="card-title text-center">${x.name}</h5>
           <div class="p-3">
+          <p class="oculto">${x.description}</p>
           <p class="card-text text-center">Date: ${x.date}</p>
           <p class="card-text text-center">Place: ${x.place}</p>
           </div>
@@ -20,15 +22,15 @@ const cards = data.events.map((x) => {
       </div>
     </div>
   `;
-}).join('');
+  }).join('');
+  card.innerHTML = cards
+}
+mostrarCards(data.events)
 
-card.innerHTML = cards
-
-//Búsqueda
+//BUSQUEDA
 const searchInput = document.getElementById('search');
 const searchButton = document.getElementById('boton');
 let busqueda = '';
-
 function filterCards() {
   const cardElements = document.querySelectorAll('.card2');
   cardElements.forEach(kard => {
@@ -53,9 +55,9 @@ searchInput.addEventListener('keypress', (e) => {
     filterCards()
   }
 });
-
 let categoria = document.getElementById("categoria");
 
+//CATEGORIA
 const categoriasUnicas = {};
 const categoriasHTML = [];
 
@@ -66,10 +68,44 @@ data.events.forEach((evento) => {
     categoriasHTML.push(
       `<div class="input-group-text">
         <label class="checkbox-inline">
-          <input type="checkbox" id="${evento._id}" value=""> ${categoria}
+          <input type="checkbox" id="${evento._id}" value="${evento.category}"> ${categoria}
         </label>
       </div>`
     );
   }
 });
 categoria.innerHTML = categoriasHTML.join("");
+
+//CHECKBOX
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let categoriasSeleccionadas = [];
+
+// Evento de click en los checkboxes
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('click', (e) => {
+    // Crea lista de categorías seleccionadas
+    if (e.target.checked && !categoriasSeleccionadas.includes(e.target.value)) {
+      categoriasSeleccionadas.push(e.target.value);
+    } else if (categoriasSeleccionadas.includes(e.target.value)) {
+      const catIndex = categoriasSeleccionadas.findIndex(cat => cat === e.target.value);
+      categoriasSeleccionadas.splice(catIndex, 1);
+    }
+    console.log(categoriasSeleccionadas);
+    // Filtra los eventos según las categorías seleccionadas
+    let eventosFiltrados = []
+    categoriasSeleccionadas.forEach(ctg => {
+      data.events.forEach(evento => {
+        if (evento.category === ctg) {
+          eventosFiltrados.push(evento)
+          console.log(eventosFiltrados);
+        }
+      })
+    })
+    if (eventosFiltrados.length === 0) {
+      mostrarCards(data.events)
+    } else {
+      mostrarCards(eventosFiltrados)
+    }
+    if (searchInput.value !== '') filterCards()
+  });
+});
